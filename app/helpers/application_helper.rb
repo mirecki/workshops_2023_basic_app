85eeeb04d92727d6f53067e4418d75a7a1269ab1
mirecki <michal.mirecki95@gmail.com>
2023-05-26 21:16:36 +0200

    Add location to weather feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 require 'capybara/rspec'
-require 'support/chromedriver'
-# require 'support/selenium_spec_firefox_remote'
+
 module ApplicationHelper
   def reserve_book_button(book, css_class: '')
     return unless book.reservation_available_for?(current_user)
--- app/services/weather_api_connector.rb
+++ app/services/weather_api_connector.rb
@@ -1,6 +1,10 @@
 class WeatherApiConnector
   API_KEY = A9n.weather_api_key
-  LOCATION = 'Cracow'.freeze
+  getIPUrl = 'http://ip-api.com/json/';
+  ipUri = URI(getIPUrl)
+  responseIp = Net::HTTP.get(ipUri)
+  LOCATION = JSON.parse(responseIp)['city'].freeze
+
   def weather_data
     url = "http://api.weatherapi.com/v1/current.json?key=#{API_KEY}&q=#{LOCATION}"
     uri = URI(url)
