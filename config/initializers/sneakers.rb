98a2a990a22e6228d85787cac41dc5d837f07366
mirecki <michal.mirecki95@gmail.com>
2023-06-03 12:36:50 +0200

    Rabbit A9n configuration

--- config/initializers/sneakers.rb
+++ config/initializers/sneakers.rb
@@ -1,6 +1,8 @@
 require 'sneakers'
 
-bunny_connection_options = { host: "localhost", port: "5672", vhost: "/", username: "guest", password: "guest" }
+bunny_connection_options = { host: A9n.bunny_host, port: A9n.bunny_port,
+ vhost: A9n.bunny_vhost, username: A9n.bunny_username,
+  password: A9n.bunny_password }
 
 Sneakers.configure(
   connection: Bunny.new(bunny_connection_options),
